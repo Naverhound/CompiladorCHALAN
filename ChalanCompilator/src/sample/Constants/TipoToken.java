68b13e5ab9d9b68d2a2726d1fdd3d390f2ccedac
Naverhound <39379474+Naverhound@users.noreply.github.com>
2018-11-14 19:59:59 -0700

    Empieza trabajo de interprete
    se agregó programación y clases necesarias para el interprete

---
+++ ChalanCompilator/src/sample/Constants/TipoToken.java
@@ -0,0 +1,20 @@
+package sample.Constants;
+
+import jdk.nashorn.internal.parser.Token;
+
+import java.util.ArrayList;
+
+public final class TipoToken {
+
+    public static  final String CREAR="CREAR";
+    public static  final String MOSTRAR="SHOW";
+    public static  final String STRING="STRING";
+    public static  final String INT="ENTERO";
+    public static  final String FOR="FOR";
+    public static  final String WHILE="WHILE";
+    public static  final String MAS="PLUS";
+    public static  final String MENOS="MINUS";
+    public static  final String MULTIPLICAR="TIMES";
+    public static  final String DIVISION="OVER";
+    public static final ArrayList<Token> Variables=new ArrayList<>();
+}
